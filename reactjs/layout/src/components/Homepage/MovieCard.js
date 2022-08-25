4c9e51ce462c94aeca30b57b33bda9c37e421adf
Gurvinder04 <kaurgurvinder19304@gmail.com>
2022-08-25 15:50:41 +0200

    updated

--- reactjs/layout/src/components/Homepage/MovieCard.js
+++ reactjs/layout/src/components/Homepage/MovieCard.js
@@ -3,9 +3,10 @@ import { Card, Col, Container, Row } from 'react-bootstrap'
 
 function MovieCard({movie}) {
   return (
+    
     <Col md ={3} className={"p-2"}>
-    <Card style={{"width":"25rem"}} className={'main main-hover '}>
-    <Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/8cXbitsS6dWQ5gfMTZdorpAAzEH.jpg" style={{"height":"80vh"}}/>
+    <Card style={{"width":"20rem"}} className={'main main-hover '}>
+    <Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/8cXbitsS6dWQ5gfMTZdorpAAzEH.jpg" style={{"height":"30vh"}}/>
     <Card.Body>
       <Card.Title className='title'>{movie.title}</Card.Title>
       <Card.Text className='overview'>
@@ -19,7 +20,6 @@ function MovieCard({movie}) {
     </Card.Footer>
   </Card>
   </Col>
-
   )
 }
 
