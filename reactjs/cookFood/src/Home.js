505ba1dfb4b4301aa660643d1b6862f303e6cd47
Gurvinder04 <kaurgurvinder19304@gmail.com>
2022-09-02 16:28:58 +0200

    cookfood card

--- reactjs/cookFood/src/Home.js
+++ reactjs/cookFood/src/Home.js
@@ -1,24 +1,57 @@
-import React from 'react'
-import {Col, Container, Row } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import FoodCard from './components/Homepage/FoodCard'
 import FoodCarousel from './components/Homepage/FoodCarousel'
 
 function Home() {
+  const [foodItems, setfoodItems] = useState([])
+
+  const fetchData = async()=> {
+    let response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
+    let data = await response.json()
+    setfoodItems(data.meals)
+  }
+  useEffect(()=>{
+       fetchData()
+  },[foodItems])
+
   return (
-    <>
-     <Container fluid={true}  className="p-0" style={{"width":"93%"}}>
-        <FoodCarousel/>
-        <Row className='text-center justify-content-center align-items-center'>
-          <Col md={6} className='text-center mt-2'>
-            <h1 style={{"font-family": "'Lobster', cursive"}}>Today's Specials</h1>
-          </Col>
-          <Col md={6}>
-            <img src='https://cdn.shopify.com/s/files/1/0551/7462/7487/products/Seafood-Lovers-Pizza-Kit-SeaBear-2-13179-2022-b_800x.jpg?v=1655228575' className='roundImg' />
-          </Col>
+  <>
+    <Container fluid={true} className="p-0" style={{ "width": "93%" }}>
+      <FoodCarousel />
+      <Row className='m-5'>
+        <Col md={6} className='text-center'>
+          <h1 style={{ "font-family": "'Lobster', cursive", "font-size": "4.5rem", "color": "red" }}>Today's Specials</h1>
+          <p><strong>2 SLICES OF CHEESE PIZZA WITH A DRINK $4.60</strong><br></br>+taxes(toppings are extra)<br></br>Please see menu</p>
+          <p><strong>1 SLICE OF CHEESE PIZZA WITH A SMALL<br></br>HOUSE SALAD & A DRINK $6.50</strong><br></br>+taxes(Toppings are extra)</p>
+          <p><strong>AVAILABLE MONDAY-SATURDAY 11AM-3PM<br></br>DINE IN OR CARRY OUT ONLY</strong></p>
+          <h1 style={{ "font-family": "'Pacifico', cursive", "font-size": "2.5rem", "color": "red" }}>limited time offer</h1>
+          <Button className='bg-danger text-light specialbtn'>ALL SPECIALS</Button>
+        </Col>
+        <Col md={6}>
+          <img src='https://cdn.shopify.com/s/files/1/0551/7462/7487/products/Seafood-Lovers-Pizza-Kit-SeaBear-2-13179-2022-b_800x.jpg?v=1655228575' className='roundImg' />
+        </Col>
         </Row>
+        <div className='shell'>
+          <div className ='container'>
+          <div className ='row'>
+          {
+            foodItems != undefined ?
+              foodItems.map((food, index) => (
+                <FoodCard food={food} key={index} />
+              ))
+              :
+              ''
+          }
+          </div>
+          </div>
+          
+          </div>
+        {
+          console.log(foodItems)
+        }
     </Container>
     </>
-    
-   
   )
 }
 
---
+++ reactjs/cookFood/src/components/Homepage/FoodCard.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+import { Col } from 'react-bootstrap'
+
+function FoodCard({ food }) {
+    useEffect(() => {
+
+    }, [food])
+    return (
+        <>
+        {/* <Col>
+            <img src={food.strMealThumb} className='image' />
+            <div className="content-box">
+                <h4 className="name">{food.strMeal}</h4>
+                <p>A burguer typically considered a sandwich, consisting of one or more cooked patties-usually placed inside a sliced bread roll or bun</p>
+                <p>Onion, Lettuce, Tomatoe, Patty, Cheese</p>
+                <div className="pricebtn">
+                    <h2 className="price">$ 5</h2>
+                    <a href="#">Order Now</a>
+                </div>
+            </div>
+        </Col> */}
+
+
+
+                    <div class="col-md-3">
+                        <div class="wsk-cp-product">
+                            <div class="wsk-cp-img">
+                                <img src={food.strMealThumb} />
+                            </div>
+                            <div class="wsk-cp-text">
+                                <div class="category">
+                                    <span>{food.strCategory}</span>
+                                </div>
+                                <div class="title-product">
+                                    <h3>{food.strMeal}</h3>
+                                </div>
+                                <div class="description-prod">
+                                    <p>{food.strInstructions}</p>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="wcf-left"><span class="price">Read more</span></div>
+                                    <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+               
+      </>
+
+    )
+}
+
+export default FoodCard
