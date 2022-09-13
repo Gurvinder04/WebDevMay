d2e4eb46bb95fbb37f9213cb9ee3d1118b361fed
Gurvinder04 <kaurgurvinder19304@gmail.com>
2022-09-13 15:45:33 +0200

    like icon set

--- reactjs/cookFood/src/components/Homepage/FoodCard.js
+++ reactjs/cookFood/src/components/Homepage/FoodCard.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Col, Container } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
+import { BiLike } from 'react-icons/bi'
 
 function FoodCard({ food, index, foodItems, category }) {
     useEffect(() => {
@@ -41,7 +42,8 @@ function FoodCard({ food, index, foodItems, category }) {
                         <div class="card-footer">
                             {/* <div class="wcf-left"><span class="price"><a href='/ReceipeDetail'>Read more</a></span></div> */}
                             <Link to={`/${food.idMeal}`} className="wcf-left a_read"><span className='price'>Get Receipe</span></Link>
-                            <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a></div>
+                            {/* <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a></div> */}
+                            <div class="wcf-right"><a href="#" class="buy-btn"><BiLike></BiLike></a></div>
                         </div>
                     </div>
                 </div>
