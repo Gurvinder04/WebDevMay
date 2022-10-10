d0e6a6ec5ac1f9cc82c10ca6649f903864324f36
Gurvinder04 <kaurgurvinder19304@gmail.com>
2022-10-10 11:43:00 +0200

    PORTFOLIO

--- reactjs/Portfolio/src/Components/Header.js
+++ reactjs/Portfolio/src/Components/Header.js
@@ -5,15 +5,15 @@ function Header() {
   return (
     < div className='bottom-bar'>
     <Navbar collapseOnSelect expand="lg"  variant="dark">
-    <Container>
+    <Container className='box'>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto vertical">
-          <Nav.Link><BsFillHouseFill className="icon"></BsFillHouseFill></Nav.Link>
-          <Nav.Link><BsFillPersonFill className="icon"></BsFillPersonFill></Nav.Link>
-          <Nav.Link><BsFillBriefcaseFill className="icon"></BsFillBriefcaseFill></Nav.Link>
-          <Nav.Link><BsChatSquareDotsFill className="icon"></BsChatSquareDotsFill></Nav.Link>
-          <Nav.Link><BsCardChecklist className="icon" ></BsCardChecklist></Nav.Link>
+          <Nav.Link className='ulli'><BsFillHouseFill className="icon"></BsFillHouseFill><p>HOME</p></Nav.Link>
+          <Nav.Link className='ulli'><BsFillPersonFill className="icon"></BsFillPersonFill><p>ABOUT</p></Nav.Link>
+          <Nav.Link className='ulli'><BsFillBriefcaseFill className="icon"></BsFillBriefcaseFill><p>PORTFOLIO</p></Nav.Link>
+          <Nav.Link className='ulli'><BsChatSquareDotsFill className="icon"></BsChatSquareDotsFill><p>CONTACT</p></Nav.Link>
+          <Nav.Link className='ulli'><BsCardChecklist className="icon"></BsCardChecklist><p>BLOG</p></Nav.Link>
          
         </Nav>
         <Nav>
--- reactjs/Portfolio/src/Home.js
+++ reactjs/Portfolio/src/Home.js
@@ -4,15 +4,15 @@ import { Col, Container, Row } from 'react-bootstrap'
 function Home() {
   return (
    <Container fluid={true} className='bg-dark' style={{ "height": "800px" }}>
-    <Row className='justify-content-center align-items-center text-center partition'>
-      <Col md={6}>
+    <Row className='justify-content-center align-items-center partition'>
+      <Col md={6} className=''>
       {/* <p className='text-light'>Hello world</p> */}
-      <img src='https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-712513.jpg&fm=jpg' style={{"width":"600px" ,"height": "600px" }}/>
+      <img src='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80' style={{"width":"550px" ,"height": "750px" }}/>
       </Col>
-      <Col md={6}>
-        <h1 className='name'>-I'M GURVINDER KAUR.</h1>
+      <Col md={6} className=''>
+        <h1 className='name'>- I'M GURVINDER KAUR.</h1>
         <h1 className='info'>WEB DESIGNER</h1>
-      <p className='text-light'>I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+      <p className='text-light fs-5'>I'm a Tunisian based web designer & front‑end developer focused on crafting clean<br></br> & user‑friendly experiences, I am passionate about building excellent software that<br></br> improves the lives of those around me.</p>
       </Col>
     </Row>
     
