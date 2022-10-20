56751bc2d155489497d2a5854ad98cea6a2db195
Gurvinder04 <kaurgurvinder19304@gmail.com>
2022-10-20 22:51:07 +0200

    about change

--- reactjs/Portfolio/src/About.js
+++ reactjs/Portfolio/src/About.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 
 function About() {
   return (
-   <Container fluid={true} className='bg-dark'>
-    <h1 className='text-light text-center about-heading'>ABOUT<span> ME</span></h1>
-    <Row className='text-light'>
-        <h2>PERSONAL INFO</h2>
-    </Row>
-   </Container>
+    <Container fluid={true} className='bg-dark'>
+      <h1 className='text-light text-center about-heading'>ABOUT<span> ME</span></h1>
+      <Row className='text-light'>
+        <h2>PERSONAL INFOS</h2>
+        <Col>
+          First Name: Steve<br></br>
+          Age: 27 Years<br></br>
+          Freelance: Available<br></br>
+          Phone: +21621184010<br></br>
+          Skype: steve.milner
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   )
 }
 
